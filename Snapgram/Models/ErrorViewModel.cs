bbd95281e9a18f99f68cc681d3d63683bccec756
Vikas-M-V <vikasmandli7@gmail.com>
2024-09-09 21:28:19 +0530

    Add project files.

---
+++ Snapgram/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Snapgram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
